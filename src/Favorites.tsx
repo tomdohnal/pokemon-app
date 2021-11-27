4a6d584c4e350b8d3ea5ca2632a1d9dc1fb53a21
Tom Dohnal <tomas.dohnal@gmail.com>
2021-11-27 23:10:29 +0100

    improve favorite page styles

--- src/Favorites.tsx
+++ src/Favorites.tsx
@@ -1,24 +1,41 @@
 import { useFavoritePokemons } from './lib/favorite-pokemons';
 import Ul from './Ul';
 import Link from './Link';
+import styled from '@emotion/styled';
+import PlaceHolder from './PlaceHolder';
+import { VoidDrawing } from './drawings';
+
+const Heading = styled.h1({
+  marginTop: 0,
+  marginBottom: '1rem',
+  fontSize: '2rem',
+});
 
 function Favorites() {
   const { favoritePokemons } = useFavoritePokemons();
 
   if (!favoritePokemons.length) {
-    return null;
+    return (
+      <PlaceHolder
+        drawing={<VoidDrawing />}
+        text="You haven't favorited any pokemons yet"
+      />
+    );
   }
 
   return (
-    <Ul>
-      {favoritePokemons.map((pokemon) => (
-        <li key={pokemon}>
-          <Link to={`/?pokemonName=${pokemon.toLocaleLowerCase()}`}>
-            {pokemon}
-          </Link>
-        </li>
-      ))}
-    </Ul>
+    <div>
+      <Heading>Favorited pokemons</Heading>
+      <Ul>
+        {favoritePokemons.map((pokemon) => (
+          <li key={pokemon}>
+            <Link to={`/?pokemonName=${pokemon.toLocaleLowerCase()}`}>
+              {pokemon}
+            </Link>
+          </li>
+        ))}
+      </Ul>
+    </div>
   );
 }
 
